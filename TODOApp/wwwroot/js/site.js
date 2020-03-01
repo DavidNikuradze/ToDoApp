8892ea1b6265bc941be96d30d3d0d3a1adf1e627
დავით ნიკურაძე <DAVID.DEEJAY.DD@GMAIL.COM>
2020-03-01 20:14:02 +0400

    ToDoApp 0.2v

--- TODOApp/wwwroot/js/site.js
+++ TODOApp/wwwroot/js/site.js
@@ -2,3 +2,19 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+$(".editBtn").click(function () {
+
+    var currentId = this.id;
+    var todoText = "#TODOText_" + currentId;
+    var todoNewText = "#TODONewText_" + currentId;
+    var delAndEdt = "#DelAndEd_" + currentId;
+    var save = "#Save_" + currentId;
+
+    $(todoText).hide();
+    $(delAndEdt).hide();
+
+    $(todoNewText).show();
+    $(save).show();
+});
